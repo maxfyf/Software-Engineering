0eee2b90e840375217f7b97abe18256d1181b6d6
Ding Hongsen <23307130300@fudan.edu.cn>
2026-03-31 11:26:47 +0800

    整合后端代码

--- backend/api.py
+++ backend/api.py
@@ -280,4 +280,4 @@ app.include_router(task_router)
 # 启动入口
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
