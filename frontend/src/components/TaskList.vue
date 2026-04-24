9922dc3d1ea7b207b629223a80b7a598ec439bc3
Xijun Xu <1109447339@qq.com>
2026-04-24 14:32:52 +0800

    feat: 修改路由结构

--- frontend/src/components/TaskList.vue
+++ frontend/src/components/TaskList.vue
@@ -89,8 +89,24 @@ const checkTask = (row) => {
 const editTask = (row) => {
   console.log('编辑任务:', row)
   highlightTaskId.value = row.id
+  
+  // 根据当前路由确定编辑路径
+  const currentPath = props.router.currentRoute?.value?.path || ''
+  let editPath = '/task/all/edit'
+  
+  // 判断是否在团队空间
+  const teamMatch = currentPath.match(/\/team\/(all|owner|admin|member)\/space/)
+  if (teamMatch) {
+    // 在团队空间，跳转到团队路由下的 edit
+    editPath = `/team/${teamMatch[1]}/space/edit`
+  } else if (currentPath.includes('/task/personal')) {
+    editPath = '/task/personal/edit'
+  } else if (currentPath.includes('/task/team')) {
+    editPath = '/task/team/edit'
+  }
+  
   props.router.push({
-    path: '/task/edit',
+    path: editPath,
     query: {
       isNew: 'false',
       taskId: row.id
