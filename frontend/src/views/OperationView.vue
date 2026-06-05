2e83355b415ef4bbb9e1da9ae545a4447b4ca237
Xijun Xu <1109447339@qq.com>
2026-06-05 12:58:47 +0800

    feat: 完善操作日志功能

--- frontend/src/views/OperationView.vue
+++ frontend/src/views/OperationView.vue
@@ -4,25 +4,20 @@ import Route from "@/components/Route.vue";
 import Search from "@/components/Search.vue";
 import OperationList from "@/components/OperationList.vue";
 import { useRoute, useRouter } from "vue-router";
-import TaskList from "@/components/TaskList.vue";
+import { useOperationView } from "@/utils/useOperationView.js";
 
 const route = useRoute()
 const router = useRouter()
 
-// TODO: 搜索框数据集（操作对象）
-const dataset = []
-
-// TODO: 搜索框回调函数
-const handleSelectObject = () => {
-
-}
-
-// TODO: operation类型数组
-const operations = []
-
-// TODO: 操作列表页面（可以类似TaskList做一个useOperationView.js）
-const currentPage = 1;
-const pageSize = 10;
+const {
+  dataset,
+  operations,
+  currentPage,
+  pageSize,
+  handleSelectObject,
+  handleSearchObject,
+  handlePageChange
+} = useOperationView()
 </script>
 
 <template>
@@ -34,7 +29,12 @@ const pageSize = 10;
         </div>
 
         <div class="search-wrapper">
-          <Search :dataset="dataset" :onSelect="handleSelectObject" />
+          <Search
+              :dataset="dataset"
+              :onSelect="handleSelectObject"
+              :showAux="true"
+              @search="handleSearchObject"
+          />
         </div>
       </div>
     </template>
@@ -44,6 +44,7 @@ const pageSize = 10;
           :operations="operations"
           :current-page="currentPage"
           :page-size="pageSize"
+          @page-change="handlePageChange"
       />
     </div>
   </HeaderWrapper>
